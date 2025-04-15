67f556473372f230acc8a6f1fd31a44ec2742ba9
Av-F <felz527@gmail.com>
2025-04-15 16:20:12 -0400

    Added in Y axis support

--- joystick_main/joystick_main.ino
+++ joystick_main/joystick_main.ino
@@ -6,21 +6,49 @@
 BleGamepad bleGamepad;
 //the x pin on the joystick is on j13, so set it in the code
 int xPin = 35;
+
+//Y axis addition
+const int deadzone = 200; // Adjust this for stick drift
+int lastDirection = 0; // -1 for up, 1 for down, 0 for neutral
+const int yAxisPin = 34; // Analog pin for Y-axis
+
 void setup() {
   // begin the program using the common standard rate
   Serial.begin(115200);
   bleGamepad.begin();
   // Set the all axis to 0
   bleGamepad.setAxes(0,0,0,0);
+  
+  analogReadResolution(12); // ESP32 default is 12 bits (0-4095)
+
 }
 
 void loop() {
   // Check if the gampead is connected
   if(bleGamepad.isConnected()) {
+   //y axis inputs
+    int rawY = analogRead(yAxisPin); // Read raw Y-axis value (0 - 4095)
+  // Map to signed 16-bit range (-32767 to 32767) expected by BleGamepad
+    int16_t yAxis = map(rawY, 0, 4095, -32767, 32767);
+
+    // Send the Y-axis update
+    bleGamepad.setLeftThumb(0, yAxis); // X=0, Y=value
+    bleGamepad.sendReport();
+
+     if (rawY > 2048 + deadzone && lastDirection != 1) {
+      Serial.println("Joystick moved DOWN");
+      lastDirection = 1;
+    } else if (rawY < 2048 - deadzone && lastDirection != -1) {
+      Serial.println("Joystick moved UP");
+      lastDirection = -1;
+    } else if (rawY >= 2048 - deadzone && rawY <= 2048 + deadzone && lastDirection != 0) {
+      Serial.println("Joystick in CENTER");
+      lastDirection = 0;
+    }
+
     int currX = analogRead(xPin); //read the current x value
     int mappedX = map(currX, 0, 4095, -32767, 32767); /*map it to how BLE likes it
     * Note that the 32767 comes from BLE itsself for the ranges */
-   
    /*This determines if we should set the x position to either left right or neutral depending on the joystick deadzone
    I decided on these values based on rigerous testing on the behavior of the joystick when I kept it in neutral as it fluctuated
    between 11400 and 9000, now I could continue to tweak the values to be to the exact, however keeping it very generalized shouldn't 
